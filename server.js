ca4a85d62e830db4bb01325dc427ba3655b94166
alexgipi <53187869+alexgipi@users.noreply.github.com>
2024-01-18 11:17:59 +0100

    Changes in server...

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 import express, { json } from "express";
 import http from "http"
-import path from "path"
 import reload from "reload";
 
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
-import multer from "multer";
 
 import { handler as ssrHandler } from "./dist/server/entry.mjs";
-import { createSlug, slugify } from "./utils.js";
-import { Collections, createCollectionEndpoints, LEXI_CONFIG } from "./lexgi.mjs"
-// import type { Collection } from "./src/types";
+import { formatOrderNumber } from "./utils.js";
+import { Collections, createCollectionEndpoints } from "./lexgi.mjs"
+
 import cors from "cors";
 import colors from "colors";
+import 'dotenv/config';
+import { sendOrderEmail } from "./email-service.js";
+import Stripe from "stripe";
+import errorHandlerMiddleware from "./middleware/handleErrors.js";
+
+// This is your test secret API key.
+const stripe = new Stripe("sk_test_Xgwk5Iu84Jw1Af7SRIzRU5ur");
 
 const app = express();
 app.use(cors());
@@ -35,12 +40,127 @@ Object.values(Collections).forEach((collection) => {
   createCollectionEndpoints(collection, collectionRouter);
 });
 
+const OrderModel = mongoose.models['orders'] || mongoose.model('orders', schema);
+console.log(OrderModel)
+
+// Stripe
+app.post("/api/create-confirm-intent", async (req, res) => {
+  const {
+    user,
+    paymentMethodId,
+    items,
+    email,
+    customerName,
+    guestCustomerId,
+    address,
+    shippingCost,
+    shippingType,
+    subtotal,
+    total
+  } = req.body;
+  // console.log({ paymentMethodId, items, email, address })
+
+  try {
+    const intent = await stripe.paymentIntents.create({
+      confirm: true,
+      amount: total*100,
+      currency: "eur",
+      // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+      automatic_payment_methods: {
+        enabled: true,
+      },
+      payment_method: paymentMethodId, // the PaymentMethod ID sent by your client
+      return_url: "https://example.com/order/123/complete",
+      use_stripe_sdk: true,
+      mandate_data: {
+        customer_acceptance: {
+          type: "online",
+          online: {
+            ip_address: req.ip,
+            user_agent: req.get("user-agent"),
+          },
+        },
+      },
+    });
+
+    const { status } = intent;
+
+    if (status === "succeeded") {
+      const ordersCount = await OrderModel.find().count({});
+      console.log(formatOrderNumber(ordersCount + 1, 6))
+      // create order
+      const newOrderData = {
+        user: user || null,
+        guestCustomerId,
+        orderNumber: formatOrderNumber(ordersCount + 1, 6),
+        items,
+        email,
+        customerName,
+        billingAddress: address,
+        shippingAddress: address,
+        paymentMethod: "stripe",
+        shippingCost,
+        shippingType,
+        subtotal,
+        total
+      };
+
+      try {
+        const order = await createOrder(newOrderData);
+
+        res.json({
+          order,
+          client_secret: intent.client_secret,
+          status: intent.status,
+        });
+      } catch (error) {
+        console.log(error)
+
+        newOrderData.orderNumber = formatOrderNumber(ordersCount + 2, 6);      
+        const order = await createOrder(newOrderData);
+
+        res.json({
+          order,
+          client_secret: intent.client_secret,
+          status: intent.status,
+        });
+      }
+
+    } else {
+      res.status(500).send({status: 'error'})
+    }
+
+    
+  } catch (err) {
+    console.log(err)
+    res.json({
+      error: err,
+    });
+  }
+});
+
 app.use(API_PREFIX, collectionRouter);
+// Dejar el middleware de manejo de errores al final de todas las rutas.
+app.use(errorHandlerMiddleware);
 
 const server = http.createServer(app)
 
+async function createOrder(order) {
+  const newOrder = new OrderModel(order);
+  await newOrder.save();
+
+  const emailOptions = {
+    fromName: "🌍 Elmundo del saquito", // sender address
+    fromEmail: "ventas@elmundodelsaquito.es",
+    to: "alexgp895@gmail.com",
+  }
+  sendOrderEmail(newOrder, emailOptions)
+
+  return newOrder;
+}
+
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/test-elmundo");
+  await mongoose.connect(process.env.DATABASE_URI);
 }
 
 main()
@@ -59,7 +179,7 @@ main()
     console.log(`${formattedDate}`.gray + ` INFO`.green + ' (Lexgi CMS): '.white + 'Connected to MongoDB server successfully!'.blue);
     console.log(`${formattedDate}`.gray + ` INFO`.green + ' (Lexgi CMS): '.white + 'Starting Lexgi CMS...'.blue);
 
-    const port = LEXI_CONFIG.port || 3400;
+    const port = process.env.PORT || 3400;
   
     reload(app).then(function (reloadReturned) {
       
