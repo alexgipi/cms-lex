e836c95e716d0e57597019c0da1c5cb6c07ee42a
alexgipi <53187869+alexgipi@users.noreply.github.com>
2024-01-18 11:15:07 +0100

    Add logic in lexgi...

--- lexgi.mjs
+++ lexgi.mjs
@@ -2,7 +2,11 @@ import { createSlug, slugify } from "./utils.js";
 import express from "express";
 import mongoose from "mongoose";
 import multer from "multer";
-import { CollectionsConfig, lexgiConfig } from "./lexgi.config.mjs";
+import autopopulate from 'mongoose-autopopulate';
+import { CollectionsConfig } from "./lexgi.config.mjs";
+import userExtractor from "./middleware/userExtractor.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -53,8 +57,6 @@ const fieldTypeTypes = {
     },
 };
 
-export const LEXI_CONFIG = lexgiConfig;
-
 export let Collections = {
   users: {
     name: "Users",
@@ -91,18 +93,20 @@ export function getCollection(collectionSlug) {
 
 export function createCollectionEndpoints(collection, router) {
     const { fields } = collection;
-  
     let newSchema = {};
     let uploadFields = [];
+
+    const cpUpload = upload.fields(uploadFields)
   
     if (fields) {
       fields.forEach((field) => {
         
-  
-        if(field.type === 'relation'){
+        if(field.type === 'relation'){         
+          const relationToSlug = slugify(field.relationTo);
           newSchema[field.name] = {
             type: fieldTypeTypes[field.type].type,
-            ref: field.relationTo
+            ref: relationToSlug,
+            autopopulate: true,
           };
   
           if(field.hasMany){
@@ -144,211 +148,318 @@ export function createCollectionEndpoints(collection, router) {
     }
   
     const schema = new Schema(newSchema, { timestamps: true });
-  
-    // Define el modelo de mongoose para la colección
+    schema.plugin(autopopulate);
+
     const Model = mongoose.models[collection.slug] || mongoose.model(collection.slug, schema);
-  
-    const cpUpload = upload.fields(uploadFields)
-    
-    router.post(`/${collection.slug}`, cpUpload, async (req, res) => {
-      try {
-        const { name, slug} = req.body;
-  
-        const files = req.files;
-        const data = req.body;
-  
-        if(files){
-          Object.keys(files).map(key => {
-            const fieldFiles = files[key];
-            collection?.fields.forEach((field) => {
-              if(field.type === 'images' || field.type === 'image'){
-                if(field.name === key){
-                  const multiple = field?.multiple;
-                  const fileNamesArray = [];
-  
-                  fieldFiles.forEach(fieldFile => {
-                    fileNamesArray.push(fieldFile.filename);
-                  })
-  
-                  if(multiple){
-                    data[key] = fileNamesArray;
-                  }else {
-                    data[key] = fileNamesArray[0];
-                  }
-  
-                  console.log({multiple})
-                }
-              }
-            })
-            
-          })
+
+    if(collection.slug === 'users'){
+      // Users
+      router.post("/users", async (req, res) => {
+        const { email, password } = req.body;
+
+        const existingUser = await Model.findOne({ email });
+
+        if (existingUser) {
+          return res.status(400).send({ ok:false, error: "User already exists" });
         }
-    
-        if(!slug || slug === undefined){
-          data.slug = slugify(name)
-        } else {
-          data.slug = slugify(slug)
+
+        const saltRounds = 10;
+        const salt = await bcrypt.genSalt(saltRounds);
+        const hash = await bcrypt.hash(password, salt);
+
+        let newUserName = email.split("@")[0] || null;
+
+        const existingUserName = await Model.findOne({ userName: newUserName });
+
+        if (existingUserName) {
+          const randomString = generateRandomString(5);
+          newUserName = newUserName + "-" + randomString;
         }
-    
-        const newDoc = new Model(data);
-        await newDoc.save();
-    
-        res.send({
-          data: newDoc
-        })
-        
-      } catch (error) {
-        console.error("Error creating " + collection.slug, error.message);
-        res.status(500).send({ 
-          error: "Error creating " + collection.slug,
-          message: error.message
+
+        const user = new Model({
+          userName: newUserName,
+          email,
+          password: hash,
         });
-      }
-      
-    });
-  
-    router.get(`/${collection.slug}`, async (req, res) => {
-      const {page, limit} = req.query;
-      const skip = (page - 1) * limit;
-    
-      const documents = await Model.find().limit(limit * 1).skip(skip);
-    
-      res.send(documents)
-    });
-  
-    router.get(`/${collection.slug}/:idOrSlug`, async (req, res) => {
-      try {
-        const { idOrSlug } = req.params;
-    
-        const query = {
-          $or: [
-            { _id: mongoose.Types.ObjectId.isValid(idOrSlug) ? idOrSlug : null },
-            { slug: idOrSlug },
-          ]
-        };
-    
-        const doc = await Model.findOne(query); // Selecciona los campos que necesitas
-    
-        if (!doc) {
-          return res.status(404).send({ error: collection.name + " no encontrado" });
-        }
-    
-        res.send(doc);
-    
-      } catch (error) {
-        console.error("Error al buscar el producto:", error.message);
-        res.status(500).send({ error: "Error interno del servidor" });
-      }
-    });
-  
-    router.patch(`/${collection.slug}/:idOrSlug`, cpUpload, async (req, res) => {
-      try {
-        const { idOrSlug } = req.params;
-        const { name, slug } = req.body;
-        const files = req.files;
-        let updateData = req.body;
-  
-        if(files){
-          Object.keys(files).map(key => {
-            const fieldFiles = files[key];
-            collection?.fields.forEach((field) => {
-              if(field.type === 'images' || field.type === 'image'){
-                if(field.name === key){
-                  const multiple = field?.multiple;
-                  const fileNamesArray = [];
-  
-                  fieldFiles.forEach(fieldFile => {
-                    fileNamesArray.push(fieldFile.filename);
-                  })
-  
-                  if(multiple){
-                    updateData[key] = fileNamesArray;
-                  }else {
-                    updateData[key] = fileNamesArray[0];
-                  }
-  
-                  console.log({multiple})
-                }
-              }
-            })
-            
-          })
-        }
-    
-    
-        const query = {
-          $or: [
-            { _id: mongoose.Types.ObjectId.isValid(idOrSlug) ? idOrSlug : null },
-            { slug: idOrSlug },
-          ]
-        };
-    
-        if(slug === undefined){
-          if(name) updateData.slug = slugify(name)
+
+        await user.save();
+
+        if (user) {
+          res.status(200).send({ ok: true, user, message: 'User created'});
         } else {
-          updateData.slug = slugify(slug)
+          res.status(400).send({ ok: false, error: "Error creating user" });
         }
-    
-        const updatedDoc = await Model.findOneAndUpdate(query, updateData, { new: true });
-        console.log({updatedDoc})
-        if (!updatedDoc) {
-          return res.status(404).send({ error: "This document not exists" });
-        }
-    
-        res.send({updatedDoc});
-    
-      } catch (error) {
-        console.error("Error al actualizar el producto:", error.message);
-        res.status(500).send({ error: "Error interno del servidor" });
-      }
-    });
-    
-  
-    // APARTIR DE AQUí es lo antiguo
-  
-    // router.get(`/${collection.slug}`, (req, res) => {
-    //   // Lógica para buscar todos los elementos de la colección
-  
-    //   Model.find({})
-    //     .then((documents) => {
-    //       res.send(documents);
-    //     })
-    //     .catch((err) => {
-    //       res.send(err);
-    //     });
-    // });
-  
-    router.get(`/${collection.slug}/:id`, (req, res) => {
-      // Lógica para buscar por ID en la colección
-      res.send({
-        type: "Find by Id",
-        collectionSlug: collection.slug,
-        collection,
-        data: [{ id: 1, name: "John Doe" }],
       });
-    });
-  
-    router.post(`/${collection.slug}`, (req, res) => {
-      // Lógica para crear un nuevo elemento en la colección
-      const requestBody = req.body;
-  
-      const collectionModel = new Model(requestBody);
-  
-      //Guardar collectionModel
-      collectionModel
-        .save()
-        .then((newArticle) => {
-          res.send("New Article Has been Added Into Your DataBase.");
-        })
-        .catch((err) => {
-          res.send(err);
+
+      router.get("/users", userExtractor, async (req, res) => {
+        const users = await Model.find({});
+
+        // sacar userId de request
+        res.send({
+          userId: req.userId,
+          users,
         });
-  
-      // res.send({
-      //   type: "Create",
-      //   collectionSlug: collection.slug,
-      //   collection,
-      //   body: requestBody,
-      // });
-    });
+      });
+
+      router.post("/users/login", async (req, res) => {
+        const { email, password } = req.body;
+
+        if (!email || !password)
+          return res.status(400).send({ ok: false, error: "Missing email or password" });
+
+        const user = await Model.findOne({ email });
+
+        if (!user) {
+          return res.status(401).send({
+            ok: false,
+            error: "Invalid user or password",
+          });
+        }
+
+        const passwordCorrect = user === null ? false : await bcrypt.compare(password, user.password);
+
+        const userForToken = {
+          id: user._id,
+          name: user.name,
+          userName: user.userName,
+          email: user.email,
+          role: user.role,
+          address: user.address,
+        };
+
+        const token = await jwt.sign(userForToken, process.env.SECRET, {
+          expiresIn: 60 * 60 * 24 * 7,
+        });
+
+        if (passwordCorrect) {
+          res.send({
+            ok: true,
+            message: "Auth Passed",
+            user: userForToken,
+            token,
+          });
+        } else {
+          res.status(401).send({
+            ok: false,
+            error: "Invalid user or password",
+          });
+        }
+      });
+      
+    } else {
+    
+      router.post(`/${collection.slug}`, cpUpload, async (req, res) => {
+        try {
+          const { name, slug} = req.body;
+    
+          const files = req.files;
+          const data = req.body;
+    
+          if(files){
+            Object.keys(files).map(key => {
+              const fieldFiles = files[key];
+              collection?.fields.forEach((field) => {
+                if(field.type === 'images' || field.type === 'image'){
+                  if(field.name === key){
+                    const multiple = field?.multiple;
+                    const fileNamesArray = [];
+    
+                    fieldFiles.forEach(fieldFile => {
+                      fileNamesArray.push(fieldFile.filename);
+                    })
+    
+                    if(multiple){
+                      data[key] = fileNamesArray;
+                    }else {
+                      data[key] = fileNamesArray[0];
+                    }
+    
+                  }
+                }
+              })
+              
+            })
+          }
+      
+          if(!slug || slug === undefined){
+            data.slug = slugify(name)
+          } else {
+            data.slug = slugify(slug)
+          }
+      
+          const newDoc = new Model(data);
+          await newDoc.save();
+      
+          res.send({
+            data: newDoc
+          })
+          
+        } catch (error) {
+          console.error("Error creating " + collection.slug, error.message);
+          res.status(500).send({ 
+            error: "Error creating " + collection.slug,
+            message: error.message
+          });
+        }
+        
+      });
+    
+      router.get(`/${collection.slug}`, async (req, res) => {
+        const {page, limit} = req.query;
+        const skip = ((page || 1) - 1) * (limit || 10);
+      
+        const documents = await Model.find();
+      
+        res.send(documents)
+      });
+    
+      router.get(`/${collection.slug}/:idOrSlug`, async (req, res) => {
+        try {
+          const { idOrSlug } = req.params;
+
+          const query = {
+            $or: [
+              { _id: mongoose.Types.ObjectId.isValid(idOrSlug) ? idOrSlug : null },
+              { slug: idOrSlug },
+            ]
+          };
+
+          const doc = await Model.findOne(query)
+      
+          if (!doc) {
+            return res.status(404).send({ error: collection.name + " no encontrado" });
+          }
+      
+          res.send(doc);
+      
+        } catch (error) {
+          console.error("Error al buscar el producto:", error.message);
+          res.status(500).send({ error: "Error interno del servidor" });
+        }
+      });
+    
+      router.patch(`/${collection.slug}/:idOrSlug`, cpUpload, async (req, res) => {
+        try {
+          const { idOrSlug } = req.params;
+          const { name, slug } = req.body;
+          const files = req.files;
+          let updateData = req.body;
+    
+          if(files){
+            Object.keys(files).map(key => {
+              const fieldFiles = files[key];
+              collection?.fields.forEach((field) => {
+                if(field.type === 'images' || field.type === 'image'){
+                  if(field.name === key){
+                    const multiple = field?.multiple;
+                    const fileNamesArray = [];
+    
+                    fieldFiles.forEach(fieldFile => {
+                      fileNamesArray.push(fieldFile.filename);
+                    })
+    
+                    if(multiple){
+                      updateData[key] = fileNamesArray;
+                    }else {
+                      updateData[key] = fileNamesArray[0];
+                    }
+                  }
+                }
+              })
+              
+            })
+          }
+      
+      
+          const query = {
+            $or: [
+              { _id: mongoose.Types.ObjectId.isValid(idOrSlug) ? idOrSlug : null },
+              { slug: idOrSlug },
+            ]
+          };
+      
+          if(slug === undefined){
+            if(name) updateData.slug = slugify(name)
+          } else {
+            updateData.slug = slugify(slug)
+          }
+      
+          const updatedDoc = await Model.findOneAndUpdate(query, updateData, { new: true });
+
+          if (!updatedDoc) {
+            return res.status(404).send({ error: "This document not exists" });
+          }
+      
+          res.send({updatedDoc});
+      
+        } catch (error) {
+          console.error("Error al actualizar el producto:", error.message);
+          res.status(500).send({ error: "Error interno del servidor" });
+        }
+      });
+
+      router.get(`/${collection.slug}/:prop/:value`, async (req, res) => {
+        try {
+          const {page, limit} = req.query;
+          const skip = ((page || 1) - 1) * (limit || 10);
+          const { prop, value } = req.params;
+
+          console.log({value})
+
+          const propArray = prop.split('.')
+          const propName = propArray[0];
+          const childProps = propArray.slice(1);
+
+          console.log({propName, childProps})
+
+          let collectionField = collection.fields.find((colField) => colField.name === propName);
+
+          if(!collectionField) {
+            return res.send({message: "La propiedad " + prop + ' no existe en la coleccion ' + collection.slug})
+          }
+
+          if(collectionField.type === 'relation' && childProps.length >= 1){
+            const relationToSlug = slugify(collectionField?.relationTo);
+            const relationModel = mongoose.models[relationToSlug];
+
+            let childCollectionFind = {}
+
+            childCollectionFind[childProps[0]] = value;
+
+            const document = await relationModel.findOne(childCollectionFind);
+
+            if(document){
+              let find = {};
+              find[propName] = document._id;
+              const documents = await Model.find(find).limit(limit);
+            
+              res.send(documents)
+            }
+          }else {
+            let find = {};
+            find[propName] = value;
+            const documents = await Model.find(find).limit(limit);
+          
+            res.send(documents)
+          }
+
+          
+
+        } catch (error) {
+          console.error("Error al buscar el producto:", error.message);
+          res.status(404).send({ error: "Producto no encontrado" });
+        }
+      });
+    }
+}
+
+function generateRandomString(length) {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let randomString = "";
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    randomString += characters.charAt(randomIndex);
+  }
+  return randomString;
 }
