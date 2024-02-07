f2c2cf961b83312b6af81299063e147cb7b354bf
alexgipi <53187869+alexgipi@users.noreply.github.com>
2024-02-07 02:38:14 +0100

    Move Users and Products to file & add export const lexiConfig

--- lexgi.config.mjs
+++ lexgi.config.mjs
@@ -1,186 +1,14 @@
+import { Users } from "./collections/Users.js";
+import  { Products }  from "./collections/Products.js"
+import { Settings } from "./collections/Settings.js";
 
 export const CollectionsConfig = [
-  {
-    name: "Users",
-    fields: [
-      {
-        name: "name",
-        type: "text",
-        default: null,
-      },
-      {
-        name: "userName",
-        type: "text",
-        required: true,
-      },
-      {
-        name: "email",
-        type: "text",
-        required: true,
-      },
-      {
-        name: "password",
-        type: "text",
-        required: true,
-      },
-      {
-        name: "role",
-        type: "text",
-        default: "customer",
-      },
-      {
-        name: "address",
-        type: "text",
-        default: null,
-      },
-    ],
-  },
-  {
-    name: "Products",
-    labels: {
-      singular: {
-        en: "Product",
-        es: "Producto",
-      },
-      plural: {
-        en: "Products",
-        es: "Productos",
-      },
-    },
-    fields: [
-      {
-        name: "name",
-        type: "text",
-        required: true,
-        label: {
-          es: "Nombre",
-          en: "Name",
-        },
-      },
-      {
-        name: "slug",
-        type: "text",
-        required: true,
-        unique: true,
-        options: {
-          sidebar: true,
-        },
-      },
-      {
-        name: "type",
-        type: "select",
-        default: "simple",
-        enum: ["simple", "variable", "downloadable"],
-        label: {
-          es: "Tipo de producto",
-          en: "Product Type",
-        },
-      },
-      {
-        name: "price",
-        type: "number",
-        default: null,
-        label: {
-          es: "Precio",
-          en: "Price",
-        },
-      },
-      {
-        name: "categories",
-        type: "relation",
-        relationTo: "Product categories",
-        hasMany: true,
-        default: null,
-        options: {
-          sidebar: true,
-        },
-        label: {
-          es: "Categorías",
-          en: "Categories",
-        },
-      },
-      {
-        name: "short_description",
-        type: "richText",
-        default: null,
-        label: {
-          es: "Descripción corta",
-          en: "Short description",
-        },
-      },
-      {
-        name: "description",
-        type: "richText",
-        default: null,
-        label: {
-          es: "Descripción",
-          en: "Description",
-        },
-      },
-      {
-        name: "image",
-        type: "image",
-        relationTo: "Media",
-        label: {
-          es: "Imagen de producto",
-          en: "Product Image",
-        },
-        default: null,
-        options: {
-          sidebar: true,
-        },
-      },
-      {
-        name: "images",
-        type: "images",
-        default: null,
-        multiple: true,
-        relationTo: "Media",
-        hasMany: true,
-        max: 10,
-        options: {
-          sidebar: true,
-        },
-        label: {
-          es: "Galería de producto",
-          en: "Product Gallery",
-        },
-      },
-      {
-        name: "attributes",
-        type: "relation",
-        relationTo: "Product attributes",
-        hasMany: true,
-        default: null,
-        label: {
-          es: "Atributos",
-          en: "Attributes",
-        },
-      },
-      {
-        name: "stock",
-        type: "number",
-        default: null,
-        options: {
-          sidebar: true,
-        },
-      },
-      {
-        name: "out_of_stock",
-        type: "boolean",
-        default: false,
-        options: {
-          sidebar: true,
-        },
-        label: {
-          es: "Fuera de 'Stock'",
-          en: "Out of Stock",
-        },
-      },
-    ],
-  },
+  Users,
+  Products,
   {
     name: "Product categories",
+    useAsTitle: "name",
+    useAsSubtitle: "slug",
     fields: [
       {
         name: "name",
@@ -459,3 +287,42 @@ export const CollectionsConfig = [
     ],
   },
 ];
+
+export const lexiConfig = {
+  collections: CollectionsConfig,
+  settings: [
+    {
+      name: "Stripe Settings",
+      slug: "stripe-settings",
+      labels: {
+        en: "Stripe Settings",
+        es: "Ajustes de Stripe",
+      },
+      fields: [
+        {
+          name: "client_token",
+          type: "text",
+          required: true,
+          // default: null,
+          label: {
+            es: "Client token",
+            en: "Client token",
+          },
+        },
+        {
+          name: "webhook_secret",
+          type: "password",
+          default: null,
+          label: {
+            es: "Webhook secret",
+            en: "Webhook secret",
+          },
+        },
+      ],
+  }
+  ],
+  dashboard: {
+    title: "El mundo del saquito",
+    short_title: "El mundo",
+  }
+}
