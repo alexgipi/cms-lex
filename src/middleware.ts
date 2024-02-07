9c8f774025d17c2f24935862f92cdaa30ba62f24
alexgipi <53187869+alexgipi@users.noreply.github.com>
2024-02-07 01:42:52 +0100

    Add identity cookie token comprobation

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,9 +3,29 @@ import { defineMiddleware } from "astro:middleware";
 export const onRequest = defineMiddleware(async (context, next) => {
   const pathname = context.url.pathname;
 
-  if (pathname === '/') {
-    return context.redirect(`/admin`);
-  } else {
-    return next();
+  let identityCookie:any = context.cookies.get('identity');
+  let identity = null;
+  let token = null;
+  const allowedPathnames = pathname === '/login' || pathname === '/registro'
+
+  if(identityCookie){
+    identity = JSON.parse(identityCookie.value)
+    token = identity.token;
   }
+
+  if(token){
+    if (pathname === '/') {
+      return context.redirect(`/admin`);
+    } else {
+      return next();
+    }
+  } else {
+    if (allowedPathnames) {
+      return next();
+    } else {
+      return context.redirect(`/login`);
+    }
+  }
+
+  
 });
