b3fe0e1c51b2aeb7b6c82bd7cd5f11511b2bf880
alexgipi <53187869+alexgipi@users.noreply.github.com>
2024-02-07 02:40:50 +0100

    Create Products collection file

---
+++ collections/Products.js
@@ -0,0 +1,145 @@
+export const Products = {
+    name: "Products",
+    useAsTitle: "name",
+    labels: {
+      singular: {
+        en: "Product",
+        es: "Producto",
+      },
+      plural: {
+        en: "Products",
+        es: "Productos",
+      },
+    },
+    fields: [
+      {
+        name: "name",
+        type: "text",
+        required: true,
+        label: {
+          es: "Nombre",
+          en: "Name",
+        },
+      },
+      {
+        name: "slug",
+        type: "text",
+        required: true,
+        unique: true,
+        options: {
+          sidebar: true,
+        },
+      },
+      {
+        name: "type",
+        type: "select",
+        default: "simple",
+        enum: ["simple", "variable", "downloadable"],
+        label: {
+          es: "Tipo de producto",
+          en: "Product Type",
+        },
+      },
+      {
+        name: "price",
+        type: "number",
+        default: null,
+        label: {
+          es: "Precio",
+          en: "Price",
+        },
+      },
+      {
+        name: "categories",
+        type: "relation",
+        relationTo: "Product categories",
+        hasMany: true,
+        default: null,
+        options: {
+          sidebar: true,
+        },
+        label: {
+          es: "Categorías",
+          en: "Categories",
+        },
+      },
+      {
+        name: "short_description",
+        type: "richText",
+        default: null,
+        label: {
+          es: "Descripción corta",
+          en: "Short description",
+        },
+      },
+      {
+        name: "description",
+        type: "richText",
+        default: null,
+        label: {
+          es: "Descripción",
+          en: "Description",
+        },
+      },
+      {
+        name: "image",
+        type: "image",
+        relationTo: "Media",
+        label: {
+          es: "Imagen de producto",
+          en: "Product Image",
+        },
+        default: null,
+        options: {
+          sidebar: true,
+        },
+      },
+      {
+        name: "images",
+        type: "images",
+        default: null,
+        multiple: true,
+        relationTo: "Media",
+        hasMany: true,
+        max: 10,
+        options: {
+          sidebar: true,
+        },
+        label: {
+          es: "Galería de producto",
+          en: "Product Gallery",
+        },
+      },
+      {
+        name: "attributes",
+        type: "relation",
+        relationTo: "Product attributes",
+        hasMany: true,
+        default: null,
+        label: {
+          es: "Atributos",
+          en: "Attributes",
+        },
+      },
+      {
+        name: "stock",
+        type: "number",
+        default: null,
+        options: {
+          sidebar: true,
+        },
+      },
+      {
+        name: "out_of_stock",
+        type: "boolean",
+        default: false,
+        options: {
+          sidebar: true,
+        },
+        label: {
+          es: "Fuera de 'Stock'",
+          en: "Out of Stock",
+        },
+      },
+    ],
+}
