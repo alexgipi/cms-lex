a0215d8f3f4d44625889af2aff1fa50c2cc8e71d
alexgipi <53187869+alexgipi@users.noreply.github.com>
2024-02-07 01:41:04 +0100

    Add DEFAULT_LEXI_SETTINGS const

--- consts.js
+++ consts.js
@@ -1,2 +1,73 @@
 export const API_URL = `http://localhost:${process.env.PORT || 3400}/api/`;
 export const siteUrl = 'http://localhost:4321';
+
+export const DEFAULT_LEXI_SETTINGS = {
+    name: "Lexi Settings",
+    slug: "lexi-settings",
+    labels: {
+      singular: {
+        en: "Product",
+        es: "Producto",
+      },
+      plural: {
+        en: "Lexi Settings",
+        es: "Ajustes de Lexi",
+      },
+    },
+    fields: [
+      {
+        name: "sitename",
+        type: "text",
+        required: true,
+        label: {
+          es: "Nombre del sitio",
+          en: "Site name",
+        },
+      },
+      {
+        name: "description",
+        type: "richText",
+        default: null,
+        label: {
+          es: "Descripción",
+          en: "Description",
+        },
+      },
+      {
+        name: "custom_css",
+        type: "richText",
+        default: null,
+        label: {
+          es: "CSS Personalizado",
+          en: "Custom CSS",
+        },
+      },
+
+      {
+        name: "sitelogo",
+        type: "image",
+        relationTo: "Media",
+        label: {
+          es: "Imagen del sitio",
+          en: "Site logo",
+        },
+        default: null,
+        options: {
+          sidebar: true,
+        },
+      },
+      {
+        name: "language",
+        type: "select",
+        default: "ES",
+        enum: ["ES", "EN"],
+        label: {
+          es: "Idioma",
+          en: "Language",
+        },
+        options: {
+            sidebar: true,
+        }
+      },
+    ],
+}
