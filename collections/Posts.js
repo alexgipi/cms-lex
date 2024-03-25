d982539a6626397ab8600c3c3586a432c14bfa98
alexgipi <53187869+alexgipi@users.noreply.github.com>
2024-03-25 09:29:00 +0100

    Create Posts collection

---
+++ collections/Posts.js
@@ -0,0 +1,109 @@
+export const Posts = {
+    name: 'posts',
+    useAsTitle: 'editor',
+    labels: {
+      singular: {
+        en: 'Post',
+        es: 'Post',
+      },
+      plural: {
+        en: 'Posts',
+        es: 'Posts',
+      },
+    },
+    fields: [
+      // Email added by default
+      {
+        name: "menu_items",
+        type: "array",
+        default: null,
+        label: {
+          es: "Elementos del menú",
+          en: "Menu items",
+        },
+        labels: {
+          singular: {
+            es: "Elemento de menú",
+            en: "Menu item",
+          },
+          plural: {
+            es: "Elementos de menú",
+            en: "Menu items",
+          },
+        },
+        fields: [
+          {
+            name: "label",
+            type: "text",
+            required: true,
+            label: {
+              es: "Label",
+              en: "Label",
+            },
+          },
+          {
+            name: "link",
+            type: "text",
+            required: true,
+            label: {
+              es: "Enlace",
+              en: "Link",
+            },
+          },
+        ],
+        options: {
+          sidebar: false,
+        },
+      },
+      {
+          name: 'editor',
+          type: 'text',
+          required: true,
+          label: {
+            en: 'Editor',
+            es: 'Editor',
+          }
+      },
+      {
+          name: 'pageMeta', // required
+          type: 'group', // required
+          fields: [
+            // required
+            {
+              name: 'title',
+              type: 'text',
+              minLength: 20,
+              maxLength: 100,
+            },
+            {
+              name: 'description',
+              type: 'textarea',
+              minLength: 40,
+              maxLength: 160,
+            },
+          ],
+      },
+      {
+        type: 'row', // required
+        fields: [
+          // required
+          {
+            name: 'label',
+            type: 'text',
+            required: true,
+            admin: {
+              width: '50%',
+            },
+          },
+          {
+            name: 'value',
+            type: 'text',
+            required: true,
+            admin: {
+              width: '50%',
+            },
+          },
+        ],
+      },
+    ],
+  }
