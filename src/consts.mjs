550512bf6a38d34d6691253a1a6a60ae3c1a59e6
alexgipi <53187869+alexgipi@users.noreply.github.com>
2024-03-25 12:34:37 +0100

    Move ADMIN_HOST const from const.js to /src/consts.mjs

--- src/consts.mjs
+++ src/consts.mjs
@@ -1,3 +1,4 @@
 export const API_URL = import.meta.env.PUBLIC_API_URL;
 export const UPLOADS_URL = import.meta.env.PUBLIC_UPLOADS_URL;
 export const SITE_URL = import.meta.env.PUBLIC_SITE_URL;
+export const ADMIN_HOST = import.meta?.env?.PUBLIC_ADMIN_HOST || 'http://localhost:4321';
